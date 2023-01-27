7ce5ea3030bc2105d9bbe440856956865ea1441c
Lisa <mkoliju@gmail.com>
2023-01-27 10:44:56 -0800

    Set up initial files

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: false,
+    module: {
+        rules: [
+            {
+
+            }]
+        }
+    }
+
+}
