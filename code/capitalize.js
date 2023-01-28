d83f9ed41484c0074152e890ea4d56c38ea1d4ab
Lisa <mkoliju@gmail.com>
2023-01-27 20:09:13 -0800

    Test if npm test works

---
+++ code/capitalize.js
@@ -0,0 +1,5 @@
+function capitalize() {
+    return 'Mandu'
+}
+
+module.exports = capitalize;
---
+++ test/capitalize.test.js
@@ -0,0 +1,5 @@
+const capitalize = require('../code/capitalize');
+
+test('capitalize', () => {
+    expect(capitalize('mandu')).toBe('Mandu')
+})
