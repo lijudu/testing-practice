1f440342b49ce36c65406e0f001122aa2bd7aeea
Lisa <mkoliju@gmail.com>
2023-01-29 09:49:48 -0800

    Capitalize test complete

--- code/capitalize.js
+++ code/capitalize.js
@@ -1,5 +1,8 @@
-function capitalize() {
-    return 'Mandu'
+function capitalize(string) {
+    let letter = string.charAt(0)
+    letter = letter.toUpperCase()
+    const newstring = letter + string.slice(1)
+    return newstring
 }
 
 module.exports = capitalize;
