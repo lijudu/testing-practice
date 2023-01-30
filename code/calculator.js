baf882d95652f0d73843f20a21eea281a4541520
Lisa <mkoliju@gmail.com>
2023-01-29 19:47:32 -0800

    Calculator test functional

--- code/calculator.js
+++ code/calculator.js
@@ -1,5 +1,16 @@
-function calculator(number) {
-
-}
+const calculator = {
+    add: function(num1, num2) {
+        return num1 + num2
+    },
+    subtract: function(num1, num2) {
+        return num1 - num2
+    },
+    multiply: function(num1, num2) {
+        return num1 * num2
+    },
+    divide: function(num1,num2){
+        return num1/num2
+    },
+};
 
 module.exports = calculator
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,5 +1,8 @@
 const calculator = require('../code/calculator');
 
 test('calculator', ()=>{
-    expect().toBe()
+    expect(calculator.add(1,2)).toBe(3);
+    expect(calculator.divide(10,2)).toBe(5);
+    expect(calculator.subtract(7,5)).toBe(2);
+    expect(calculator.multiply(3,3)).toBe(9);
 })
